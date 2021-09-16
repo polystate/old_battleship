59e40f8828568d59b2d24ec76b66eb00c3dc3a52
polystate <dockadams333@gmail.com>
2021-09-16 12:45:08 +0700

    Tests refactored

--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -10,6 +10,24 @@ function placeOrigin(length,origin){
     return playerGrid;
 }
 
+function shipPlacementVertical(length,origin,name){
+    let gridArr = gameBoard().gridArr;
+    gridArr[origin] = JSON.stringify(Ship(length,name));
+    if((origin - 1) + (length * 10) > 99) return;
+    for(let i = 10; i < length*10; i+=10){
+        gridArr[origin + i] = gridArr[origin];
+    }
+    return gridArr;
+}
+
+function shipPlacementHorizontal(length,origin,name){
+    let gridArr = gameBoard().gridArr;
+    gridArr[origin] = JSON.stringify(Ship(length,name));
+    for(let i = 1; i < length; i++){
+        gridArr[origin + i] = gridArr[origin];
+        }
+    return gridArr;
+}
 
 //Tests
 
@@ -25,20 +43,10 @@ function placeOrigin(length,origin){
     
     describe('{actually place} ship on origin with vertical alignment and length and return array', () => {
         test('place ship of length-3 at origin-8 vertically', () => {
-            let gridArr = gameBoard().gridArr;
-            gridArr[8] = JSON.stringify(Ship(3));
-            gridArr[18] = JSON.stringify(Ship(3));
-            gridArr[28] = JSON.stringify(Ship(3));
-            expect(gameBoard().placeShip(3,8,'vertical')).toEqual(gridArr);
+            expect(gameBoard().placeShip(3,8,'vertical')).toEqual(shipPlacementVertical(3,8));
         })
         test('place ship of length-5 at origin-26 vertically', () => {
-            let gridArr = gameBoard().gridArr;
-            gridArr[26] = JSON.stringify(Ship(5));
-            gridArr[36] = JSON.stringify(Ship(5));
-            gridArr[46] = JSON.stringify(Ship(5));
-            gridArr[56] = JSON.stringify(Ship(5));
-            gridArr[66] = JSON.stringify(Ship(5));
-            expect(gameBoard().placeShip(5,26,'vertical')).toEqual(gridArr);
+            expect(gameBoard().placeShip(5,26,'vertical')).toEqual(shipPlacementVertical(5,26));
         })
         test(`place ship of length-2 at origin 80 vertical should return undefined and nothing should happen because it is out of bounds`, () => {
             expect(gameBoard().placeShip(3,80,'vertical')).toEqual(undefined);
@@ -53,32 +61,14 @@ function placeOrigin(length,origin){
 
     describe('{actually place} ship on origin with horizontal alignment and length and return array', () => {
         test('place ship of length-4 at origin-2 horizontally', () => {
-            let gridArr = gameBoard().gridArr;
-            gridArr[2] = JSON.stringify(Ship(4));
-            gridArr[3] = JSON.stringify(Ship(4));
-            gridArr[4] = JSON.stringify(Ship(4));
-            gridArr[5] = JSON.stringify(Ship(4));
-            expect(gameBoard().placeShip(4,2,'horizontal')).toEqual(gridArr);
+            
+            expect(gameBoard().placeShip(4,2,'horizontal')).toEqual(shipPlacementHorizontal(4,2));
         })
         test('place ship of length-2 at origin-10 horizontally', () => {
-            let gridArr = gameBoard().gridArr;
-            gridArr[10] = JSON.stringify(Ship(2));
-            gridArr[11] = JSON.stringify(Ship(2));
-            expect(gameBoard().placeShip(2,10,'horizontal')).toEqual(gridArr);
+            expect(gameBoard().placeShip(2,10,'horizontal')).toEqual(shipPlacementHorizontal(2,10));
         })
         test('place ship of length-10 at origin-20 horizontally', () => {
-            let gridArr = gameBoard().gridArr;
-            gridArr[20] = JSON.stringify(Ship(10));
-            gridArr[21] = JSON.stringify(Ship(10));
-            gridArr[22] = JSON.stringify(Ship(10));
-            gridArr[23] = JSON.stringify(Ship(10));
-            gridArr[24] = JSON.stringify(Ship(10));
-            gridArr[25] = JSON.stringify(Ship(10));
-            gridArr[26] = JSON.stringify(Ship(10));
-            gridArr[27] = JSON.stringify(Ship(10));
-            gridArr[28] = JSON.stringify(Ship(10));
-            gridArr[29] = JSON.stringify(Ship(10));
-            expect(gameBoard().placeShip(10,20,'horizontal')).toEqual(gridArr);
+            expect(gameBoard().placeShip(10,20,'horizontal')).toEqual(shipPlacementHorizontal(10,20));
         })
         test('any ship who exceeds the horizontal edgecases of any multiple of 9 to 99 [i.e. 9 for first origin row, 19 for second origin row, 29 for third origin row, etc.] has gone out of bounds horizontally and it should return undefined', () => {
             expect(gameBoard().placeShip(8,6,'horizontal')).toEqual(undefined);
@@ -94,14 +84,8 @@ function placeOrigin(length,origin){
 
 
     describe('place a ship at certain position and give it a name as well', () => {
-        test('give the name [Courier] to ship placed at origin 12 with length of 3', () => {
-            const shipName = 'Courier';
-            let gridArr = gameBoard().gridArr;
-            gridArr[12] = JSON.stringify(Ship(3,shipName));
-            gridArr[13] = JSON.stringify(Ship(3,shipName));
-            gridArr[14] = JSON.stringify(Ship(3,shipName));
-
-            expect(gameBoard().placeShip(3,12,'horizontal',shipName)).toEqual(gridArr);
+        test('give the name [Courier] to ship placed at origin 12 with length of 3 horizontally', () => {
+            expect(gameBoard().placeShip(3,12,'horizontal','Courier')).toEqual(shipPlacementHorizontal(3,12,'Courier'));
         })
     })
     
